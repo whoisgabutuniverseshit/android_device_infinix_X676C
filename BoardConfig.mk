32694d80373193087ca1b7dd2f1c302b3a64ea3a
dantmnf <dantmnf2@gmail.com>
2023-02-15 23:07:24 +0800

    X676C: Enable adaptive refresh rate
    * Use conservative idle timer to reduce flickering
    
    Change-Id: Ibdacf6f04a790d955a8d44912926191194f9168c
    Signed-off-by: dodyirawan85 <dev.irawans@gmail.com>
    Signed-off-by: fjrXTR <fajarslebew31@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -42,6 +42,9 @@ BOARD_VENDOR_KERNEL_MODULES := $(wildcard $(KERNEL_PATH)/modules/*.ko)
 # Workaround to make lineage's soong generator work
 TARGET_KERNEL_SOURCE := $(KERNEL_PATH)/kernel-headers
 
+# Properties
+TARGET_PRODUCT_PROP += $(DEVICE_PATH)/configs/properties/product.prop
+
 # Include the common OEM chipset BoardConfig.
 include device/transsion/mt6789-common/BoardConfigCommon.mk
 
