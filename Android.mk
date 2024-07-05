a9f3729cf3a40662f28b6d95524951a50bb5cd56
fjrXTR <fajarslebew31@gmail.com>
2024-07-05 10:52:44 +0000

    X676C: Import Audio Stack
    Change-Id: I1406adfaeb9a5e60af1151562efa18caffc6cf2a
    Signed-off-by: fjrXTR <fajarslebew31@gmail.com>

--- Android.mk
+++ Android.mk
@@ -7,5 +7,20 @@
 LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_DEVICE),X676C)
-include $(call all-subdir-makefiles,$(LOCAL_PATH))
+
+include $(call all-makefiles-under,$(LOCAL_PATH))
+
+AUDIO_SYMLINKS := \
+	$(TARGET_OUT_VENDOR)/lib/hw/audio.primary.$(TARGET_BOARD_PLATFORM).so \
+	$(TARGET_OUT_VENDOR)/lib/hw/audio.r_submix.$(TARGET_BOARD_PLATFORM).so \
+	$(TARGET_OUT_VENDOR)/lib64/hw/audio.primary.$(TARGET_BOARD_PLATFORM).so \
+	$(TARGET_OUT_VENDOR)/lib64/hw/audio.r_submix.$(TARGET_BOARD_PLATFORM).so
+
+$(AUDIO_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
+	$(hide) echo "Linking $@"
+	@ln -sf $(subst $(TARGET_BOARD_PLATFORM),mediatek,$(notdir $@)) $@
+
+ALL_DEFAULT_INSTALLED_MODULES += \
+	$(AUDIO_SYMLINKS)
+
 endif
